3cddb77e28be37d0e24cecad39590dde38b94a44
Daniel Flores García <danialefloresg@gmail.com>
2021-07-22 09:38:12 -0400

    Add lspector's propeller as dependency

--- project.clj
+++ project.clj
@@ -7,7 +7,7 @@
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [org.clojure/clojurescript "1.10.764"]
                  [reagent "0.10.0"]
-                 [org.clojars.dfloresgarcia/propeller "0.1.1-SNAPSHOT"]]
+                 [net.clojars.lspector/propeller "0.2.0"]]
 
   :plugins [[lein-cljsbuild "1.1.7"]
             [lein-figwheel "0.5.20"]]
